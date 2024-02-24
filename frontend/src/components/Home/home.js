88631ff378ed3d014e52894189e2c60fd4299e24
Thinal Perera <thinaltp@gmail.com>
2024-02-24 09:31:27 +0530

    added home, login and register page UI

---
+++ frontend/src/components/Home/home.js
@@ -0,0 +1,27 @@
+import React, { Fragment, useEffect } from "react";
+import { Button } from "flowbite-react";
+
+const Component = () => {
+    useEffect(() => {
+        document.title = "Home";
+    }, []);
+    
+    return (
+        <Fragment>
+            <div className="h-screen flex flex-col justify-center items-center">
+                <div className="relative">
+                    <h1 className="text-[calc(128px)] font-black uppercase">To Do</h1>
+                    <div className="absolute bottom-10 w-[calc(100%+40px)] h-10 translate-x-[calc(-20px)] flex justify-center items-center bg-white/40 backdrop-blur-md">
+                        <p className="text-xl font-bold">Your Tasking Companion</p>
+                    </div>
+                </div>
+                <div className="mt-10 flex gap-5">
+                    <Button href="/login" color="dark">Login</Button>
+                    <Button href="/register" color="dark">Register</Button>
+                </div>
+            </div>
+        </Fragment>
+    );
+};
+
+export default Component;
---
+++ frontend/src/components/Login/login.js
@@ -0,0 +1,37 @@
+import React, { Fragment, useEffect } from "react";
+import { Button, Label, TextInput } from "flowbite-react";
+import { Link } from "react-router-dom";
+
+const Component = () => {
+    useEffect(() => {
+        document.title = "Login";
+    }, []);
+
+
+
+    return (
+        <Fragment>
+            <div className="h-screen flex flex-col justify-center items-center">
+                <div className="max-w-xs w-full flex flex-col gap-5">
+                    <div className="p-5 flex flex-col gap-5 bg-gray-900 rounded-lg">
+                        <h1 className="text-white text-2xl font-bold">Login</h1>
+                        <div className="text-white space-y-2">
+                            <Label htmlFor="email" color="white">Email</Label>
+                            <TextInput id="email" label="Email" type="email" placeholder="Email" />
+                        </div>
+                        <div className="text-white space-y-2">
+                            <Label htmlFor="password" color="white">Password</Label>
+                            <TextInput id="password" label="Password" type="password" placeholder="Password" />
+                        </div>
+                        <div className="w-full flex items-end justify-between gap-5 mt-5">
+                            <Button color="light">Login</Button>
+                            <Link to="/register" className="text-white hover:underline text-right">Create an account ?</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    );
+};
+
+export default Component;
