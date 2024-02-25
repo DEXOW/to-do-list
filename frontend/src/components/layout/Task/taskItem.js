e8141f04b21b3b6f25e63ede036611a22f0a5523
Thinal Perera <thinaltp@gmail.com>
2024-02-26 00:15:56 +0530

    added edit task func

--- frontend/src/components/layout/Task/taskItem.js
+++ frontend/src/components/layout/Task/taskItem.js
@@ -1,10 +1,13 @@
+import { useEffect, useState } from 'react';
 import $ from 'jquery';
-import { useEffect } from 'react';
-import { Checkbox } from 'flowbite-react';
 import axios from 'axios';
-import { IoMdMore } from 'react-icons/io';
+import { Checkbox, Dropdown } from 'flowbite-react';
+import { IoMdMore, IoIosClose } from 'react-icons/io';
+import { LuArrowRight } from "react-icons/lu";
+import { isEmpty } from 'validator';
 
 const Component = ({ task, getTaskFunc }) => {
+    const [taskName, setTaskName] = useState("");
 
     useEffect(() => {
        $(`#checkBox-${task._id}`).prop('defaultChecked', task.completed);
@@ -22,16 +25,60 @@ const Component = ({ task, getTaskFunc }) => {
         }
     }
 
+    const toggleEditTaskInput = () => {
+        $(`#taskNameDisplay-${task._id}`).toggle('50');
+        $(`#taskNameEdit-${task._id}`).toggle('50');
+    }
+
+    const toggleEditTaskSubmitBtn = (e) => {
+        if (isEmpty(e.target.value) || e.target.value === task.name) {
+            $(`#editTaskSubmitBtn-${task._id}`).hide();
+            $(`#editTaskCloseBtn${task._id}`).show();
+        } else {
+            $(`#editTaskSubmitBtn-${task._id}`).show();
+            $(`#editTaskCloseBtn${task._id}`).hide();
+        }
+    }
+
+    const handleEditTask = async () => {
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_API_URL}/task/update`, { id: task._id, name: taskName }, { withCredentials: true });
+            getTaskFunc();
+            toggleEditTaskInput();
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <div className="w-full flex justify-between bg-white rounded-lg px-4 py-3 hover:shadow-md ease-in-out duration-500">
-            <div className="flex items-center gap-4">
-                <Checkbox id={`checkBox-${task._id}`} onClick={handleTaskCheck}/>
-                <p className="text-sm">{task.name}</p>
+        <div className="w-full bg-white rounded-lg px-4 py-3 hover:shadow-md ease-in-out duration-500">
+            <div id={`taskNameDisplay-${task._id}`} className='flex justify-between'>
+                <div className="flex items-center gap-4">
+                    <Checkbox id={`checkBox-${task._id}`} onClick={handleTaskCheck}/>
+                    <p className="text-sm">{task.name}</p>
+                </div>
+                <div>
+                    <Dropdown placement="bottom" dismissOnClick={true} renderTrigger={() => 
+                        <button className="bg-white text-black rounded-lg p-2 hover:bg-gray-200 ease-in-out duration-200">
+                            <IoMdMore size={20}/>
+                        </button>
+                    }>
+                        <Dropdown.Item onClick={toggleEditTaskInput}>Edit</Dropdown.Item>
+                        <Dropdown.Item onClick={() => console.log("Delete")}>Delete</Dropdown.Item>
+                    </Dropdown>
+                </div>
             </div>
-            <div>
-                <button className="bg-white text-black rounded-lg p-2 hover:bg-gray-200 ease-in-out duration-200">
-                    <IoMdMore size={20}/>
-                </button>
+            <div id={`taskNameEdit-${task._id}`} className='hidden'>
+                <div className="flex justify-between items-center">
+                    <input type="text" defaultValue={task.name} onChange={(e) => { toggleEditTaskSubmitBtn(e); setTaskName(e.target.value) }} className="bg-transparent text-black border-0 outline-0 ring-0 focus:border-0 focus:outline-0 focus:ring-0 w-full"/>
+                    <button onClick={handleEditTask} id={`editTaskSubmitBtn-${task._id}`} className="hidden bg-white rounded-full p-2 hover:bg-gray-200 ease-in-out duration-200 cursor-pointer">
+                        <LuArrowRight strokeWidth={2.5}/>
+                    </button>
+                    <button onClick={toggleEditTaskInput} id={`editTaskCloseBtn${task._id}`} className="bg-white rounded-full p-2 hover:bg-gray-200 ease-in-out duration-200 cursor-pointer">
+                        <IoIosClose strokeWidth={2.5}/>
+                    </button>
+                </div>
             </div>
         </div>
     )
